ced5bb7edc14e28286b3fac4ca40ef4b54248fa3
hafizfaizan97 <165157377+hafizfaizan97@users.noreply.github.com>
2024-11-07 02:12:07 +0500

    # Import Anime , Fetch Data => 2 APIs:  => store in db  => fetch from json

--- app/Services/AnimeService.php
+++ app/Services/AnimeService.php
@@ -18,28 +18,41 @@ class AnimeService
     public function importTopAnime(): void
     {
         try {
-            $response = Http::timeout(10)->get('https://api.jikan.moe/v4/top/anime', ['limit' => 100]);
-            $animes = $response->json('data');
+            $totalAnime = 100;
+            $limit = 25;
+            $pages = ceil($totalAnime / $limit);
+            for ($page = 1; $page <= $pages; $page++) {
+                $response = Http::timeout(10)->get('https://api.jikan.moe/v4/top/anime', [
+                    'limit' => $limit,
+                    'page' => $page,
+                ]);
 
-            foreach ($animes as $anime) {
-                $data = [
-                    'mal_id' => $anime['mal_id'],
-                    'titles' => [
-                        'en' => $anime['title_english'] ?? $anime['title'],
-                        'pl' => $anime['title']
-                    ],
-                    'slugs' => [
-                        'en' => \Str::slug($anime['title_english'] ?? $anime['title']),
-                        'pl' => \Str::slug($anime['title'])
-                    ],
-                ];
+                if ($response->failed()) {
+                    throw new \Exception("Failed to fetch data from Jikan API: " . $response->body());
+                }
 
-                $this->animeRepository->saveAnime($data);
+                $animes = $response->json('data');
+                foreach ($animes as $anime) {
+                    $data = [
+                        'mal_id' => $anime['mal_id'],
+                        'titles' => [
+                            'en' => $anime['title_english'] ?? $anime['title'],
+                            'pl' => $anime['title']
+                        ],
+                        'slugs' => [
+                            'en' => \Str::slug($anime['title_english'] ?? $anime['title']),
+                            'pl' => \Str::slug($anime['title'])
+                        ],
+                    ];
+
+                    $this->animeRepository->saveAnime($data);
+                }
             }
         } catch (RequestException $e) {
             throw new \Exception("Failed to fetch data from Jikan API: " . $e->getMessage());
         }
     }
+
 
     public function findAnimeBySlugAndLang(string $slug, string $lang)
     {
